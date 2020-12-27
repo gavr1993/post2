2feba02766b9ca8d9cdc283eed5171bf7bd09612
GAVR <axotnick@bk.ru>
2020-12-27 14:08:47 +0300

    Api error

--- app/src/main/java/com/example/firstPost/MainActivity.kt
+++ app/src/main/java/com/example/firstPost/MainActivity.kt
@@ -4,17 +4,30 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity(R.layout.activity_main), CoroutineScope by MainScope() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val list =
-            with(container) {
-                layoutManager = LinearLayoutManager(this@MainActivity)
-                adapter = PostAdapter().apply {
-                    this.list = list
-                    notifyItemRangeInserted(0, list.size)
-                }
+        setContentView(R.layout.activity_main)
+        fetchData()
+        with(container) {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = PostAdapter().apply {
+                this.list = list
+                notifyItemRangeInserted(0, list.size)
             }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cancel()
+    }
+
+    fun fetchData() = launch {
+        val list = withContext(Dispatchers.IO) {
+            Api.client.get<List<Post>>(Api.url)
+        }
     }
 }
