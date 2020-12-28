6bb7f8a5dab5cb5f9c12b99fddac2516ed8d5c61
GAVR <axotnick@bk.ru>
2020-12-28 14:59:05 +0300

    finished

--- app/src/main/java/com/example/firstPost/MainActivity.kt
+++ app/src/main/java/com/example/firstPost/MainActivity.kt
@@ -1,24 +1,20 @@
 package com.example.firstPost
-
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import io.ktor.client.request.*
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.*
-
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity(R.layout.activity_main), CoroutineScope by MainScope() {
-    private lateinit var list: List<Post>
-
+    private val adapter = PostAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         fetchData()
-        with(container) {
-            layoutManager = LinearLayoutManager(this@MainActivity)
-            adapter = PostAdapter().apply {
-                notifyItemRangeInserted(0, list.size)
-            }
-        }
+        container.layoutManager = LinearLayoutManager(this)
+        container.adapter = adapter
     }
 
     override fun onDestroy() {
@@ -26,9 +22,9 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), CoroutineScope b
         cancel()
     }
 
-    fun fetchData() = launch {
-        list = withContext(Dispatchers.IO) {
-            Api.client.get<List<Post>>(Api.url)
-        }
+    private fun fetchData() = launch {
+        val list = Api.client.get<List<Post>>(Api.url)
+        adapter.list = list
+        adapter.notifyItemRangeInserted(0, list.size)
     }
 }
